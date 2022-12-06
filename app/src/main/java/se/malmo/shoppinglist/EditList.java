ad691404563abdb4bf14b1d0c5b271adfa09376b
stinapz <115546113+stinapz@users.noreply.github.com>
2022-12-06 14:35:12 +0100

    Update design

---
+++ app/src/main/java/se/malmo/shoppinglist/EditList.java
@@ -0,0 +1,14 @@
+package se.malmo.shoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_list);
+    }
+}
