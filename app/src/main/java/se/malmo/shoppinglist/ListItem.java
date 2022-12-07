016f5a4f49ff586123698179e8cc03ad2b59b693
Debbie <debbie.grothen@gmail.com>
2022-12-07 11:32:03 +0100

    Klass för ListItem

--- app/src/main/java/se/malmo/shoppinglist/ListItem.java
+++ app/src/main/java/se/malmo/shoppinglist/ListItem.java
@@ -2,26 +2,40 @@ package se.malmo.shoppinglist;
 
 public class ListItem {
 
+    //fields
     private int id;
     private String item;
+    private Boolean isChecked;
 
+    //Konstruktor
     public ListItem() {
         item = "";
     }
 
-    public ListItem(int id, String item) {
+    public ListItem(int id, String item, boolean isChecked) {
         this.id = id;
         this.item = item;
+        this.isChecked = isChecked;
+
 
     }
 
     public int getId() {
             return id;
-        }
+    }
 
     public ListItem setId(int id) {
         this.id = id;
         return this;
+    }
+    public boolean getIsChecked(){
+        return isChecked;
+    }
+
+    public ListItem setIsChecked(boolean isChecked){
+        this.isChecked = isChecked;
+        return this;
+
     }
     public String getItem() {
         return item;
