a79bab218dafa647944c801679b15689a4fb507c
mChramer <106435380+mChramer@users.noreply.github.com>
2022-12-07 13:52:21 +0100

    Databas

--- app/src/main/java/se/malmo/shoppinglist/DbHelper.java
+++ app/src/main/java/se/malmo/shoppinglist/DbHelper.java
@@ -1,4 +1,36 @@
 package se.malmo.shoppinglist;
 
-public class DbHelper {
-}
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+        public static final String DB_NAME = "Shopping.db";
+        public static final int VERSION = 1;
+        public static final String TABLE_NAME = "Items";
+
+        public DbHelper(@Nullable Context context ) {
+            super(context, DB_NAME, null, VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String query =
+                    "CREATE TABLE Items (" +
+                            "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                            "item TEXT," +
+                            "isChecked BOOL" +
+                            ")";
+
+            db.execSQL(query);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS Items");
+            onCreate(db);
+        }
+    }
--- app/src/main/java/se/malmo/shoppinglist/MainActivity.java
+++ app/src/main/java/se/malmo/shoppinglist/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
