10e6fb40cf82106c686cd81788f70635c8feb0b3
Debbie <debbie.grothen@gmail.com>
2022-12-07 10:07:08 +0100

    Klass för ListItem

--- app/src/main/java/se/malmo/shoppinglist/ListItem.java
+++ app/src/main/java/se/malmo/shoppinglist/ListItem.java
@@ -1,4 +1,35 @@
 package se.malmo.shoppinglist;
 
 public class ListItem {
+
+    private int id;
+    private String item;
+
+    public ListItem() {
+        item = "";
+    }
+
+    public ListItem(int id, String item) {
+        this.id = id;
+        this.item = item;
+
+    }
+
+    public int getId() {
+            return id;
+        }
+
+    public ListItem setId(int id) {
+        this.id = id;
+        return this;
+    }
+    public String getItem() {
+        return item;
+    }
+
+    public ListItem setItem(String item) {
+        this.item = item;
+        return this;
+    }
+
 }
